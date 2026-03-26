0e694196eb8f3f844bd8c15666f401096617aa70
Sanika kalyani <sanikakalyani77@gmail.com>
2026-03-26 20:13:07 +0530

    find_max.py

--- find_max.py
+++ find_max.py
@@ -6,3 +6,9 @@ def find_max(nums):
         if nums[num]>max_value:
             max_value=num
     return max_value
+
+#test cases
+#1.nums=[1,7,3]
+#max_value[0]=1   nums[1]>max_value  7>1  true  max_value=7
+#max_value[1]=7   nums[2]>max_value  3>7  false max_value=7
+#max_value=7
