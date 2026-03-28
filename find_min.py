4b4e14c71d6e9d94df319b21bdd8ad84ca2fcba7
Sanika kalyani <sanikakalyani77@gmail.com>
2026-03-28 08:13:15 +0530

    find_min.py

--- find_min.py
+++ find_min.py
@@ -6,3 +6,9 @@ def find_min(nums):
         if nums[num]<min_value:
             min_value=num
     return min_value
+
+#test cases
+#1.nums=[1,7,-1]
+#min_value[0]=1   nums[1]<min_value  7<1  false  min_value=1
+#min_value[1]=1   nums[2]<min_value  -1<1  true  min_value=-1
+#min_value=-1
